4fde7ccf32e7f6d4503af54d64c0daec3e9b24ef
Thomas Ortiz <tdortiz@ncsu.edu>
2017-10-23 17:02:14 -0400

    Made installScript to get new VM running quicker

---
+++ installScript.sh
@@ -0,0 +1,21 @@
+# Install git
+sudo apt-get install git
+mkdir Github
+cd Github
+
+# Clone dotfiles from Github
+git clone https://github.com/Tdortiz/dotfiles
+cd dotfiles
+cp .bashrc ~
+
+# Update packages
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install build-essential
+
+# Install curl and vim
+sudo apt-get install curl
+sudo apt-get install vim
+
+# Grab vim package
+curl http://j.mp/spf13-vim3 -L -o - | sh
